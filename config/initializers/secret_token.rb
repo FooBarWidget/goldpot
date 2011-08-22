c9a3481e6339bcec27ea1dfa263e6fc3c54d287a
Hongli Lai (Phusion) <hongli@phusion.nl>
2011-08-22 14:23:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goldpot::Application.config.secret_token = '1afc634276c5fc2a6c805d4ee90f92528cc7f1988b84523c935a81b2b31005d7c5424eba13c975b2d7ac3faca4a4c90658d9e8eec06dd1d216eff9748436efc1'
